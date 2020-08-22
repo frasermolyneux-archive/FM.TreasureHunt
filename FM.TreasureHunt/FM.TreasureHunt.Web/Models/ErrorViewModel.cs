9e9a9a4f8a2dd4b7da6c63afd0a85dbc48e8e11c
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-08-22 21:20:27 +0100

    Initial commit of template

---
+++ FM.TreasureHunt/FM.TreasureHunt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FM.TreasureHunt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
