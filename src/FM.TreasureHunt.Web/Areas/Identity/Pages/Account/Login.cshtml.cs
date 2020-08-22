e28cdf2a33f5e1d4e6c8ef8ab1b87ec734a50dc0
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-08-23 00:37:20 +0100

    Fixing login

--- src/FM.TreasureHunt.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/FM.TreasureHunt.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -58,7 +58,7 @@ namespace FM.TreasureHunt.Web.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result =
-                    await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, false);
+                    await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -83,7 +83,7 @@ namespace FM.TreasureHunt.Web.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required] [EmailAddress] public string Email { get; set; }
+            [Required] public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
